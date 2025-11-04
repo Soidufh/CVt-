1d2262cf87ac0b691fc48087f183a64bda22f260
Soidufh <carl.randmets@rak.ee>
2025-11-04 14:13:38 +0200

    pilt fade

--- js/node.js
+++ js/node.js
@@ -4,7 +4,7 @@ const path = require('path');
 const imagesFolder = path.join(__dirname, '../images/portraits');
 
 const album = fs.readdirSync (imagesFolder)
-.filter(file => /\.(jpg|jpg|png|gif)$/i.test(file))
+.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file))
 .map(file => `images/portraits/${file}`);
 
 const jsContent = `const album = ${JSON.stringify(album, null, 2)}`;
